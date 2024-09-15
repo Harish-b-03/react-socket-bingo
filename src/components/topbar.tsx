1565178c367b6574676b0027a7917170123272df
Harish-b-03 <harishbalaji369@gmail.com>
2024-09-15 18:05:47 +0530

    removed copy-to-clipboard package

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,28 +1,35 @@
-import { CopyToClipboard } from "react-copy-to-clipboard";
 import { toast } from "react-toastify";
 import ClipboardIcon from "./icons/clipboard-icon";
 import { useUserContext } from "../contexts/user-context";
 
-const Topbar = () => {
+const Topbar: React.FC = () => {
 	const { user } = useUserContext();
-	if(!user) return;
-	
+	if (!user) return;
+
+	const copyRoomIDText = `RoomID: ${user.roomId}`;
+
+	const copyToClipboard = async () => {
+		try {
+			await window.navigator.clipboard.writeText(copyRoomIDText);
+			toast.success("Room ID copied");
+		} catch (err) {
+			console.error("Unable to copy to clipboard.", err);
+			toast.error("Copy to clipboard failed.");
+		}
+	};
+
 	return (
 		<div className="w-[300px]">
-			<CopyToClipboard
-				text={`RoomID: ${user.roomId}`}
-				onCopy={() => {
-					toast("RoomID copied");
-				}}
+			<button
+				className="py-1 text-sm flex items-center float-right text-gray-600 font-light cursor-pointer"
+				onClick={copyToClipboard}
 			>
-				<div className="py-1 text-sm flex items-center float-right text-gray-600 font-light cursor-pointer">
-					{/* <span className="-mb-1">{user.userName}</span> */}
-					<span>roomId: {user.roomId}</span>
-					<div className="w-[14px] h-[14px]">
-						<ClipboardIcon />
-					</div>
+				{/* <span className="-mb-1">{user.userName}</span> */}
+				<span>roomId: {user.roomId}</span>
+				<div className="w-[14px] h-[14px]">
+					<ClipboardIcon />
 				</div>
-			</CopyToClipboard>
+			</button>
 		</div>
 	);
 };
