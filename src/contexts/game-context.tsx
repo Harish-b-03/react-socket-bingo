6ad696ff423a89627e853d6718ef26983623921c
Harish-b-03 <harishbalaji369@gmail.com>
2024-10-20 16:23:59 +0530

    fix: added useBoardLogic hook

--- src/contexts/game-context.tsx
+++ src/contexts/game-context.tsx
@@ -1,21 +1,14 @@
-import React, {
-	ReactElement,
-	createContext,
-	useContext,
-	useState,
-} from "react";
+import React, { ReactElement, createContext, useContext, useState } from "react";
 import { shuffle } from "../helpers/helper";
 
 interface GameContextType {
 	shuffledData: number[][];
-	marked: number[];
 	gameStarted: boolean;
 	myTurn: boolean;
 	statusMessage: string; // this state variable is used to store turn message, like "Your turn", "Player1's turn", and win message like "You win" and "You Lost"
 	lastChecked: number | null;
 	gameOver: boolean; // we can't use gameStarted variable to check whether the game is over or not, as we have many game status like "player not ready and game not started". "player ready", "game started". So, using this variable.
 	updateShuffledData: () => void;
-	updateMarked: (val: number[]) => void;
 	updateGameStarted: (val: boolean) => void;
 	updateMyTurn: (val: boolean) => void;
 	updateStatusMessage: (val: string) => void;
@@ -26,14 +19,12 @@ interface GameContextType {
 
 const initialGameState: GameContextType = {
 	shuffledData: shuffle(),
-	marked: Array(25).fill(0),
 	gameStarted: false,
 	myTurn: false,
 	statusMessage: "",
 	lastChecked: null,
 	gameOver: false,
 	updateShuffledData: () => {},
-	updateMarked: () => {},
 	updateGameOver: () => {},
 	updateGameStarted: () => {},
 	updateMyTurn: () => {},
@@ -44,16 +35,13 @@ const initialGameState: GameContextType = {
 
 const GameStateContext = createContext<GameContextType>(initialGameState);
 
-export const GameStateProvider: React.FC<{ children: ReactElement }> = ({
-	children,
-}) => {
+export const GameStateProvider: React.FC<{ children: ReactElement }> = ({ children }) => {
 	const [lastChecked, setLastChecked] = useState<number | null>(null);
 	const [shuffledData, setShuffledData] = useState<number[][]>(shuffle());
 	const [gameStarted, setGameStarted] = useState<boolean>(false);
 	const [myTurn, setMyTurn] = useState<boolean>(false);
 	const [statusMessage, setStatusMessage] = useState<string>(""); // this state variable is used to store turn message, like "Your turn", "Player1's turn", and win message like "You win" and "You Lost"
 	const [gameOver, setGameOver] = useState<boolean>(false); // we can't use gameStarted variable to check whether the game is over or not, as we have many game status like "player not ready and game not started". "player ready", "game started". So, using this variable.
-	const [marked, setMarked] = useState<number[]>(Array(25).fill(0));
 
 	const updateLastChecked = (value: number) => setLastChecked(value);
 	const updateShuffledData = () => setShuffledData(shuffle());
@@ -61,18 +49,14 @@ export const GameStateProvider: React.FC<{ children: ReactElement }> = ({
 	const updateMyTurn = (value: boolean) => setMyTurn(value);
 	const updateStatusMessage = (value: string) => setStatusMessage(value);
 	const updateGameOver = (value: boolean) => setGameOver(value);
-	const updateMarked = (value: number[]) => setMarked(value);
 
-	const resetMarked = () => {
-		updateMarked(Array(25).fill(0));
-	};
 	const resetGame = () => {
 		setGameStarted(false);
 		setLastChecked(null);
 		setMyTurn(false);
 		setStatusMessage("");
 		setGameOver(false);
-		resetMarked();
+		console.log("resetting game ");
 	};
 
 	return (
@@ -84,14 +68,12 @@ export const GameStateProvider: React.FC<{ children: ReactElement }> = ({
 				myTurn,
 				statusMessage,
 				gameOver,
-				marked,
 				updateLastChecked,
 				updateShuffledData,
 				updateGameStarted,
 				updateMyTurn,
 				updateStatusMessage,
 				updateGameOver,
-				updateMarked,
 				resetGame,
 			}}
 		>
